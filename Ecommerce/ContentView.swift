88164d8edf6558d5cbb097aebe2aae8462850ac8
shehanhasanga <shehanhasanga.15@cse.mrt.ac.lk>
2022-02-09 15:22:48 +0530

    Initial Commit

---
+++ Ecommerce/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Ecommerce
+//
+//  Created by shehan karunarathna on 2022-02-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Ecommerce/EcommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceApp.swift
+//  Ecommerce
+//
+//  Created by shehan karunarathna on 2022-02-09.
+//
+
+import SwiftUI
+
+@main
+struct EcommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
