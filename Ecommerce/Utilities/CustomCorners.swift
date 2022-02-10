88e5b0638576c824803ba4aff6b7040950dfea32
shehanhasanga <shehanhasanga.15@cse.mrt.ac.lk>
2022-02-10 09:31:40 +0530

    added new corners

---
+++ Ecommerce/Utilities/CustomCorners.swift
@@ -0,0 +1,20 @@
+//
+//  CustomCorners.swift
+//  Ecommerce
+//
+//  Created by shehan karunarathna on 2022-02-09.
+//
+
+import Foundation
+import SwiftUI
+
+struct CustomCorners: Shape{
+    var corners:UIRectCorner
+    var radius: CGFloat
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+    
+    
+}
